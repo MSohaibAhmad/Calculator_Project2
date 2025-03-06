085e407d4f650c4d49ef8ff8c3b5b41a684bd0d0
MSohaibAhmad <syedsohaibahmad4u@gmail.com>
2025-03-06 13:48:47 +0500

    Update addition calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -6,5 +6,13 @@ class Program
     {
         Console.WriteLine("Name: Muhammad Sohaib Ahmad");
         Console.WriteLine("Roll Number: 24-ST-040");
+         
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Sum: {a + b}");
     }
 }
