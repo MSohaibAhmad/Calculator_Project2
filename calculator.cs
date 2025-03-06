5f655236c033e6164cc1e802e16d2c867e549785
MSohaibAhmad <syedsohaibahmad4u@gmail.com>
2025-03-06 13:52:53 +0500

    Update divide calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -6,5 +6,19 @@ class Program
     {
         Console.WriteLine("Name: Muhammad Sohaib Ahmad");
         Console.WriteLine("Roll Number: 24-ST-040");
+        
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (b != 0)
+        {
+            Console.WriteLine($"Quotient: {a / (double)b}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
     }
 }
